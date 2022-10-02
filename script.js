db08e46cddde37dbf9afe3138c913c80fd9d1ba4
Mlekce <stojanac.srdjan@gmail.com>
2022-10-02 19:54:22 +0200

    rock paper siccsors with gui

--- script.js
+++ script.js
@@ -1,66 +1,102 @@
-'use strict'
-//Variables
 let playerScore = 0;
 let computerScore = 0;
-const computer = function(){
-    const choices = ['rock', 'paper', 'siccssors'];
-    const randomizer = Math.trunc(Math.random()*3);
-    return choices[randomizer];
+let counter = 0;
+const rock = document.querySelectorAll(".rock");
+const paper = document.querySelectorAll(".paper");
+const siccssors = document.querySelectorAll(".siccssors");
+const buttons = document.querySelectorAll("button");
+const playerChoice = document.querySelector(".playerChoice");
+const computerChoice = document.querySelector(".computerChoice");
+const resoultsPlayer = document.querySelector(".resoultsPlayer");
+const resoultsComputer = document.querySelector(".resoultsComputer");
+const status = document.querySelector(".status");
+const final = document.querySelector(".finalScore");
+const body = document.body;
+const container = document.querySelector(".container");
+
+const computer = function () {
+  const choices = ["rock", "paper", "siccssors"];
+  const randomizer = Math.trunc(Math.random() * 3);
+  return choices[randomizer];
 };
-const player = function(){
-    let choice = prompt("Enter rock, paper or siccssors.").toLowerCase();
-    if (!choice || (choice !== 'rock' && choice !== 'paper' && choice !== 'siccssors')){
-        const choices = ['rock', 'paper', 'siccssors'];
-        const randomizer = Math.trunc(Math.random()*3);
-        return choices[randomizer];
-    }
-    else
-        return choice;
-};
-//Main game      
-function game() {
-    for(let i = 0; i < 5; i++){
-        playRound(player(),computer());
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function playRound() {
+    let playerSelection = button.getAttribute("class");
+    const computerSelection = computer();
+    counter++;
+    console.log(`Rounds played ${counter}`);
+    if (counter <= 5) {
+      if (playerSelection == "rock" && computerSelection == "siccssors") {
+        playerScore++;
+        playerChoice.innerText = `Player choice was: ${playerSelection}`;
+        computerChoice.innerText = `Computer choice was: ${computerSelection}`;
+        resoultsPlayer.innerText = `Player score is: ${playerScore}`;
+        resoultsComputer.innerText = `Computer score is: ${computerScore}`;
+        status.textContent = "Round resoult: You won!";
+        return playerScore, computerScore;
+      } 
+      else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore++;
+        playerChoice.innerText = `Player choice was: ${playerSelection}`;
+        computerChoice.innerText = `Computer choice was: ${computerSelection}`;
+        resoultsPlayer.innerText = `Player score is: ${playerScore}`;
+        resoultsComputer.innerText = `Computer score is: ${computerScore}`;
+        status.textContent = "Round resoult: You won!";
+        return playerScore, computerScore;
+      } 
+      else if (
+        playerSelection == "siccssors" && computerSelection == "paper") {
+        playerScore++;
+        playerChoice.innerText = `Player choice was: ${playerSelection}`;
+        computerChoice.innerText = `Computer choice was: ${computerSelection}`;
+        resoultsPlayer.innerText = `Player score is: ${playerScore}`;
+        resoultsComputer.innerText = `Computer score is: ${computerScore}`;
+        status.textContent = "Round resoult: You won!";
+        return playerScore, computerScore;
+      } 
+      else if (playerSelection == computerSelection) {
+        playerScore++;
+        computerScore++;
+        playerChoice.innerText = `Player choice was: ${playerSelection}`;
+        computerChoice.innerText = `Computer choice was: ${computerSelection}`;
+        resoultsPlayer.innerText = `Player score is: ${playerScore}`;
+        resoultsComputer.innerText = `Computer score is: ${computerScore}`;
+        status.textContent = "Round resoult: Draw!";
+        return playerScore, computerScore;
+      } 
+      else {
+        computerScore++;
+        playerChoice.innerText = `Player choice was: ${playerSelection}`;
+        computerChoice.innerText = `Computer choice was: ${computerSelection}`;
+        resoultsPlayer.innerText = `Player score is: ${playerScore}`;
+        resoultsComputer.innerText = `Computer score is: ${computerScore}`;
+        status.textContent = "Round resoult: You lost!";
+        return playerScore, computerScore;
+      }
     }
     winner(playerScore, computerScore);
-}        
-//Game RPS    
-function playRound(playerSelection, computerSelection){            
-    if (playerSelection == 'rock' && computerSelection == 'siccssors'){
-        playerScore++;
-        console.log(`You won player choice was ${playerSelection}, computer choice was ${computerSelection}, player score is ${playerScore}, computer score is ${computerScore}`);
-        return playerScore, computerScore;
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        playerScore++;
-        console.log(`You won player choice was ${playerSelection}, computer choice was ${computerSelection}, player score is ${playerScore}, computer score is ${computerScore}`);
-        return  playerScore, computerScore;
-    }
-    else if (playerSelection == 'siccssors' && computerSelection == 'paper'){
-        playerScore++;
-        console.log(`You won player choice was ${playerSelection}, computer choice was ${computerSelection}, player score is ${playerScore}, computer score is ${computerScore}`);
-        return playerScore, computerScore;
-    }
-    else if (playerSelection == computerSelection ){
-        console.log(`DRAW! player choice was ${playerSelection}, computer choice was ${computerSelection}, player score is ${playerScore}, computer score is ${computerScore}`);
-        return playerScore, computerScore;
-    }
-    else {
-        computerScore++;
-        console.log(`You lost player choice was ${playerSelection}, computer choice was ${computerSelection}, player score is ${playerScore}, computer score is ${computerScore}`);
-        return playerScore, computerScore;
-    }
-} 
-//Check winner function
-function winner(player, computer){
-    if(player > computer){
-        console.log(`You won ${player}, computer score ${computer}`);
-     }
-    else if (player < computer){
-        console.log(`You lost ${player}, computer score ${computer}`);
-    }
-    else {
-        console.log(`Draw. ${player}, computer score ${computer}`);
-    }
+  });
+});
+
+function winner(player, computer) {
+  if (player > computer) {
+    final.textContent = `You won. player score: ${player}, computer score: ${computer}`;
+    container.classList.add("hidden");
+    body.style.cssText =
+      "background-color:rgb(94, 182, 138); color:white; font-size:65px; margin: auto; text-align: center;margin-top:20%;";
+    body.textContent = "You won!";
+  } else if (player < computer) {
+    final.textContent = `You lost. player score:  ${player}, computer score: ${computer}`;
+    container.classList.add("hidden");
+    body.style.cssText =
+      "background-color:rgb(182, 94, 116);color:white; font-size:65px; margin: auto; text-align: center;margin-top:20%;";
+    body.textContent = "You lost!";
+  } else {
+    final.textContent = `Draw! player score:  ${player}, computer score: ${computer}`;
+    container.classList.add("hidden");
+    body.style.cssText =
+      "background-color:rgb(182, 181, 94); color:white; font-size:65px; margin: auto; text-align: center; margin-top:20%;";
+    body.textContent = "Draw!";
+  }
 }
-game();
